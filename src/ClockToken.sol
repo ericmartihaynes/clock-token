89c3ddb8aa187e8c7530b122c77cee6facc222be
ericmartihaynes <55204838+ericmartihaynes@users.noreply.github.com>
2024-06-29 21:14:49 +0200

    Logic & tests done

--- src/ClockToken.sol
+++ src/ClockToken.sol
@@ -4,9 +4,36 @@ pragma solidity 0.8.25;
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract ClockToken is ERC20 {
-    constructor() ERC20("Clock Token", "HOUR") {}
+    mapping(address => int256) public hourOffset;
+    mapping(address => int256) public minuteOffset;
+
+    constructor() ERC20("Current Time", "CLOCK") {}
 
     function balanceOf(address account) public view override returns (uint256) {
-        return block.timestamp;
+        int256 offset = hourOffset[account] * int256(10 ** decimals()) + minuteOffset[account] * int256(10 ** (decimals() - 2));
+        uint256 secondsToday = (block.timestamp % 1 days);
+        uint256 time = secondsToday / 1 hours * 10 ** decimals() + (secondsToday % 1 hours) / 1 minutes * 10 ** (decimals() - 2);
+        return  uint256(int256(time) + offset);
+    }
+
+    function offsetHours(int256 _hours) external {
+        int256 newOffset = hourOffset[msg.sender] + _hours;
+        require(newOffset >= -11 && newOffset <= 12, "Offset not allowed");
+        hourOffset[msg.sender] = newOffset;
+    }
+
+    function offsetMinutes(int256 _minutes) external {
+        int256 newOffset = minuteOffset[msg.sender] + _minutes;
+        require(newOffset >= -29 && newOffset <= 30, "Offset not allowed");
+        minuteOffset[msg.sender] = newOffset;
+    }
+
+    function resetOffsets() external {
+        delete hourOffset[msg.sender];
+        delete minuteOffset[msg.sender];
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return type(uint256).max;
     }
 }
--- test/ClockToken.t.sol
+++ test/ClockToken.t.sol
@@ -12,7 +12,113 @@ contract ClockTokenTest is Test {
     }
 
     function testBalance() public {
-        //counter.increment();
-        //assertEq(counter.number(), 1);
+        vm.warp(1719615204);
+        //console.log(clockToken.balanceOf(address(0)));
+        assertEq(clockToken.balanceOf(address(0)), 22530000000000000000);
+    }
+
+    function testBalance(address _address) public {
+        vm.warp(1719615204);
+        assertEq(clockToken.balanceOf(address(_address)), 22530000000000000000);
+    }
+
+    function testOffset() public {
+        vm.warp(1719615204);
+        address user = address(0x25);
+        vm.startPrank(user);
+        assertEq(clockToken.balanceOf(user), 22530000000000000000);
+        assertEq(clockToken.hourOffset(user), 0);
+        assertEq(clockToken.minuteOffset(user), 0);
+
+        clockToken.offsetHours(-1);
+        assertEq(clockToken.balanceOf(user), 21530000000000000000);
+        assertEq(clockToken.hourOffset(user), -1);
+        assertEq(clockToken.minuteOffset(user), 0);
+
+        clockToken.offsetHours(2);
+        assertEq(clockToken.balanceOf(user), 23530000000000000000);
+        assertEq(clockToken.balanceOf(address(0)), 22530000000000000000);
+        assertEq(clockToken.hourOffset(user), 1);
+        assertEq(clockToken.minuteOffset(user), 0);
+
+        clockToken.offsetMinutes(-23);
+        assertEq(clockToken.balanceOf(user), 23300000000000000000);
+        assertEq(clockToken.hourOffset(user), 1);
+        assertEq(clockToken.minuteOffset(user), -23);
+
+        clockToken.offsetMinutes(15);
+        assertEq(clockToken.balanceOf(user), 23450000000000000000);
+        assertEq(clockToken.balanceOf(address(0)), 22530000000000000000);
+        assertEq(clockToken.hourOffset(user), 1);
+        assertEq(clockToken.minuteOffset(user), -8);
+
+        clockToken.resetOffsets();
+        assertEq(clockToken.balanceOf(user), clockToken.balanceOf(address(0)));
+        assertEq(clockToken.hourOffset(user), 0);
+        assertEq(clockToken.minuteOffset(user), 0);
+        vm.stopPrank();
+    }
+
+    function testOffsetFails() public {
+        vm.warp(1719615204);
+        address user = address(0x25);
+        vm.startPrank(user);
+        assertEq(clockToken.balanceOf(user), 22530000000000000000);
+
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetHours(-12);
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetHours(13);
+        clockToken.offsetHours(6);
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetHours(8);
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetHours(-18);
+
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetMinutes(-30);
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetMinutes(31);
+        clockToken.offsetMinutes(20);
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetMinutes(11);
+        vm.expectRevert("Offset not allowed");
+        clockToken.offsetMinutes(-50);
+        
+        vm.stopPrank();
+    }
+
+    function testSupply() public {
+        assertEq(clockToken.totalSupply(), type(uint256).max);
+    }
+
+    function testTransfersFail() public {
+        vm.warp(1719615204);
+        address user = address(0x25);
+        address user2 = address(0x26);
+        vm.startPrank(user);
+        assertEq(clockToken.balanceOf(user), 22530000000000000000);
+
+        vm.expectRevert();
+        clockToken.transfer(user2, 22530000000000000000);
+
+        vm.expectRevert();
+        clockToken.transfer(user2, 1);
+
+        //transfering 0 does not revert
+        clockToken.transfer(user2, 0);
+
+        clockToken.approve(user2, 22530000000000000000);
+        vm.stopPrank();
+        vm.startPrank(user2);
+        vm.expectRevert();
+        clockToken.transferFrom(user, user2, 22530000000000000000);
+
+        vm.expectRevert();
+        clockToken.transferFrom(user, user2, 1);
+
+        //transfering 0 does not revert
+        clockToken.transferFrom(user, user2, 0);
+        vm.stopPrank();
     }
 }
